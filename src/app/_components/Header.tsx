9b742ec23f78205d486883de9ebb2f200dd31f82
tsavou <theosavourat@gmail.com>
2025-10-21 11:29:11 +0200

    Add Prettier configuration and update dependencies; implement Header component

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="h-15 w-full bg-gray-400">
+      <div className="flex h-full items-center justify-center">
+        <h1 className="text-2xl font-bold">Y social</h1>
+      </div>
+    </header>
+  );
+}
